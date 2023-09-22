acd29756de5e600363015aa91bc58f3761019f5a
DarkHorseCoder <120799088+DarkHorseCorder@users.noreply.github.com>
2023-09-22 16:54:19 +0200

    change footer design

--- components/Footer.js
+++ components/Footer.js
@@ -7,9 +7,9 @@ const Footer = () => {
         <Row>
           <Col>
             <div className="Footer-Credit">
-              <p className="small text-light mb-2">
+              {/* <p className="small text-light mb-2">
                 Developed by Andrii Tertychnyi @2023 . <a href="https://www.linkedin.com/in/andriiter/" className="text-info">LinkedIn</a>
-              </p>
+              </p> */}
               <Image fluid src="./static/google-logo.png" alt="Powered by Google" />
               <p className="small text-white-50 mb-0">Powered by Google</p>
             </div>
